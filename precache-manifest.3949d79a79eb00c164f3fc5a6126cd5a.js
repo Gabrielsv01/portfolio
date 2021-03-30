d6cd1c90849bcf4d40221a2a8b89b3be5ee6c335
Gabrielsv01 <gabrielsv01@gmail.com>
2021-03-29 21:44:28 -0300

    Updates

---
+++ precache-manifest.3949d79a79eb00c164f3fc5a6126cd5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79bcbced132bbbaafb410d15a588918",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f8b9f55a8122404374cb",
+    "url": "/portfolio/static/js/2.8625e7fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.8625e7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1e562449931ae5dea9",
+    "url": "/portfolio/static/js/main.0c04149a.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "b4ba9e0968a8e34f0f0f3d689207d5f8",
+    "url": "/portfolio/static/media/CurvaHomePage.b4ba9e09.svg"
+  },
+  {
+    "revision": "39965589b575d517e467ac8ce71c29e8",
+    "url": "/portfolio/static/media/Eu.39965589.jpg"
+  },
+  {
+    "revision": "e02bf79023e6035c040224253d1c9c62",
+    "url": "/portfolio/static/media/GitHub.e02bf790.svg"
+  },
+  {
+    "revision": "84c2e98b1b048104938334a85d6dd16a",
+    "url": "/portfolio/static/media/Instagram.84c2e98b.svg"
+  },
+  {
+    "revision": "f7f92903ed9fd102e80c0dd5c5aff793",
+    "url": "/portfolio/static/media/floresta.f7f92903.png"
+  }
+]);
